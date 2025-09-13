f30befa1fabf241bc397197c1c7cc26db5a84474
DarkShadow44 <dark.shadow4@web.de>
2025-09-13 10:24:26 +0200

    Simplify renderer (#23)

--- src/main/java/com/creativemd/littletiles/client/render/LittleTilesBlockRenderHelper.java
+++ src/main/java/com/creativemd/littletiles/client/render/LittleTilesBlockRenderHelper.java
@@ -14,7 +14,6 @@ import org.lwjgl.opengl.GL11;
 
 import com.creativemd.creativecore.client.block.IBlockAccessFake;
 import com.creativemd.creativecore.client.rendering.ExtendedRenderBlocks;
-import com.creativemd.creativecore.client.rendering.RenderHelper3D;
 import com.creativemd.creativecore.common.utils.ColorUtils;
 import com.creativemd.creativecore.common.utils.CubeObject;
 import com.creativemd.littletiles.LittleTiles;
@@ -38,50 +37,26 @@ public class LittleTilesBlockRenderHelper {
         fake.world = renderer.blockAccess;
 
         for (int i = 0; i < cubes.size(); i++) {
-
-            if (cubes.get(i).icon != null) renderer.setOverrideBlockTexture(cubes.get(i).icon);
-
-            if (cubes.get(i).block != null) if (cubes.get(i).meta != -1) {
+            final CubeObject cube = cubes.get(i);
+            if (cube.block != null && cube.meta != -1) {
                 extraRenderer.clearOverrideBlockTexture();
-                extraRenderer.setRenderBounds(
-                        cubes.get(i).minX,
-                        cubes.get(i).minY,
-                        cubes.get(i).minZ,
-                        cubes.get(i).maxX,
-                        cubes.get(i).maxY,
-                        cubes.get(i).maxZ);
-                extraRenderer.meta = cubes.get(i).meta;
-                fake.overrideMeta = cubes.get(i).meta;
-                extraRenderer.color = cubes.get(i).color;
+                extraRenderer.setRenderBounds(cube.minX, cube.minY, cube.minZ, cube.maxX, cube.maxY, cube.maxZ);
+                extraRenderer.meta = cube.meta;
+                fake.overrideMeta = cube.meta;
+                extraRenderer.color = cube.color;
                 extraRenderer.lockBlockBounds = true;
                 if (LittleTiles.angelicaCompat != null) {
-                    LittleTiles.angelicaCompat.setShaderMaterialOverride(cubes.get(i).block, cubes.get(i).meta);
+                    LittleTiles.angelicaCompat.setShaderMaterialOverride(cube.block, cube.meta);
                 }
                 extraRenderer.field_152631_f = true;
-                extraRenderer.renderBlockAllFaces(cubes.get(i).block, x, y, z);
+                extraRenderer.renderBlockAllFaces(cube.block, x, y, z);
                 extraRenderer.field_152631_f = false;
                 if (LittleTiles.angelicaCompat != null) {
                     LittleTiles.angelicaCompat.resetShaderMaterialOverride();
                 }
                 extraRenderer.lockBlockBounds = false;
                 extraRenderer.color = ColorUtils.WHITE;
-                continue;
-            } else renderer.setOverrideBlockTexture(cubes.get(i).block.getBlockTextureFromSide(0));
-
-            renderer.setRenderBounds(
-                    cubes.get(i).minX,
-                    cubes.get(i).minY,
-                    cubes.get(i).minZ,
-                    cubes.get(i).maxX,
-                    cubes.get(i).maxY,
-                    cubes.get(i).maxZ);
-
-            if (direction != null && direction != ForgeDirection.EAST && direction != ForgeDirection.UNKNOWN)
-                RenderHelper3D.applyBlockRotation(renderer, direction);
-
-            renderer.renderStandardBlock(block, x, y, z);
-
-            if (cubes.get(i).icon != null || cubes.get(i).block != null) renderer.clearOverrideBlockTexture();
+            }
         }
     }
 
@@ -89,85 +64,52 @@ public class LittleTilesBlockRenderHelper {
             int meta) {
         Tessellator tesselator = Tessellator.instance;
         for (int i = 0; i < cubes.size(); i++) {
+            final CubeObject cube = cubes.get(i);
             int metadata = 0;
-            if (cubes.get(i).meta != -1) metadata = cubes.get(i).meta;
+            if (cube.meta != -1) metadata = cube.meta;
             Block block = parBlock;
             if (block instanceof BlockAir) block = Blocks.stone;
-            renderer.setRenderBounds(
-                    cubes.get(i).minX,
-                    cubes.get(i).minY,
-                    cubes.get(i).minZ,
-                    cubes.get(i).maxX,
-                    cubes.get(i).maxY,
-                    cubes.get(i).maxZ);
-            if (cubes.get(i).block != null && !(cubes.get(i).block instanceof BlockAir)) {
-                block = cubes.get(i).block;
+            renderer.setRenderBounds(cube.minX, cube.minY, cube.minZ, cube.maxX, cube.maxY, cube.maxZ);
+            if (cube.block != null && !(cube.block instanceof BlockAir)) {
+                block = cube.block;
                 meta = 0;
             }
-            if (cubes.get(i).icon != null) {
-                GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
-                tesselator.startDrawingQuads();
-                tesselator.setNormal(0.0F, -1.0F, 0.0F);
-                renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, cubes.get(i).icon);
-                tesselator.draw();
-                tesselator.startDrawingQuads();
-                tesselator.setNormal(0.0F, 1.0F, 0.0F);
-                renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, cubes.get(i).icon);
-                tesselator.draw();
-                tesselator.startDrawingQuads();
-                tesselator.setNormal(0.0F, 0.0F, -1.0F);
-                renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, cubes.get(i).icon);
-                tesselator.draw();
-                tesselator.startDrawingQuads();
-                tesselator.setNormal(0.0F, 0.0F, 1.0F);
-                renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, cubes.get(i).icon);
-                tesselator.draw();
-                tesselator.startDrawingQuads();
-                tesselator.setNormal(-1.0F, 0.0F, 0.0F);
-                renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, cubes.get(i).icon);
-                tesselator.draw();
-                tesselator.startDrawingQuads();
-                tesselator.setNormal(1.0F, 0.0F, 0.0F);
-                renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, cubes.get(i).icon);
-                tesselator.draw();
-                GL11.glTranslatef(0.5F, 0.5F, 0.5F);
-            } else {
-                int j = block.getRenderColor(metadata);
-                if (cubes.get(i).color != ColorUtils.WHITE) j = cubes.get(i).color;
 
-                float f1 = (float) (j >> 16 & 255) / 255.0F;
-                float f2 = (float) (j >> 8 & 255) / 255.0F;
-                float f3 = (float) (j & 255) / 255.0F;
-                float brightness = 1.0F;
-                GL11.glColor4f(f1 * brightness, f2 * brightness, f3 * brightness, 1.0F);
+            int j = block.getRenderColor(metadata);
+            if (cube.color != ColorUtils.WHITE) j = cube.color;
 
-                GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
-                tesselator.startDrawingQuads();
-                tesselator.setNormal(0.0F, -1.0F, 0.0F);
-                renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, metadata));
-                tesselator.draw();
-                tesselator.startDrawingQuads();
-                tesselator.setNormal(0.0F, 1.0F, 0.0F);
-                renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(1, metadata));
-                tesselator.draw();
-                tesselator.startDrawingQuads();
-                tesselator.setNormal(0.0F, 0.0F, -1.0F);
-                renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(2, metadata));
-                tesselator.draw();
-                tesselator.startDrawingQuads();
-                tesselator.setNormal(0.0F, 0.0F, 1.0F);
-                renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(3, metadata));
-                tesselator.draw();
-                tesselator.startDrawingQuads();
-                tesselator.setNormal(-1.0F, 0.0F, 0.0F);
-                renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(4, metadata));
-                tesselator.draw();
-                tesselator.startDrawingQuads();
-                tesselator.setNormal(1.0F, 0.0F, 0.0F);
-                renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(5, metadata));
-                tesselator.draw();
-                GL11.glTranslatef(0.5F, 0.5F, 0.5F);
-            }
+            float f1 = (float) (j >> 16 & 255) / 255.0F;
+            float f2 = (float) (j >> 8 & 255) / 255.0F;
+            float f3 = (float) (j & 255) / 255.0F;
+            float brightness = 1.0F;
+            GL11.glColor4f(f1 * brightness, f2 * brightness, f3 * brightness, 1.0F);
+
+            GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
+            tesselator.startDrawingQuads();
+            tesselator.setNormal(0.0F, -1.0F, 0.0F);
+            renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, metadata));
+            tesselator.draw();
+            tesselator.startDrawingQuads();
+            tesselator.setNormal(0.0F, 1.0F, 0.0F);
+            renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(1, metadata));
+            tesselator.draw();
+            tesselator.startDrawingQuads();
+            tesselator.setNormal(0.0F, 0.0F, -1.0F);
+            renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(2, metadata));
+            tesselator.draw();
+            tesselator.startDrawingQuads();
+            tesselator.setNormal(0.0F, 0.0F, 1.0F);
+            renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(3, metadata));
+            tesselator.draw();
+            tesselator.startDrawingQuads();
+            tesselator.setNormal(-1.0F, 0.0F, 0.0F);
+            renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(4, metadata));
+            tesselator.draw();
+            tesselator.startDrawingQuads();
+            tesselator.setNormal(1.0F, 0.0F, 0.0F);
+            renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(5, metadata));
+            tesselator.draw();
+            GL11.glTranslatef(0.5F, 0.5F, 0.5F);
         }
     }
 
